e78b60c3eb069b5a5ec3823250957fdd316cdff8
Artem Borodenko <artyomborodenko@gmail.com>
2022-08-31 13:58:25 +0300

    initial commit

---
+++ alaya/healthcheck/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@permission_classes([])
+def health_check(request):
+    return Response('Service is available')
+# Create your views here.
